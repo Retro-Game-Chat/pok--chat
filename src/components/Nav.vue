3881d7f8789656759ec2ae403de0205a43138768
Luke Oliff <luke@lukeoliff.com>
2020-08-04 11:30:07 +0100

    game stuff

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="Nav">
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div>
+        <a href="#" class="Nav__download">Download</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<style scoped>
+.Nav {
+  @apply flex items-center justify-between flex-wrap bg-teal-500 p-6;
+}
+.Nav__download {
+  @apply inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0;
+}
+</style>
---
+++ src/components/PlaySpace.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="PlayBox">
+    <div class="PlayArea">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlaySpace'
+}
+</script>
+
+<style scoped>
+.PlayBox {
+  @apply box-content h-32 w-64 p-4 border-4 border-gray-400 bg-gray-200;
+}
+
+.PlayArea {
+  @apply h-full w-full bg-gray-400;
+}
+</style>
