ab38f558362ed41da22323425a463d0271791b57
Luke Oliff <luke@lukeoliff.com>
2020-08-06 08:22:50 +0100

    add service check back in to routed app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view v-if="!!server.status && server.status === 'ok'" />
+    <template v-else>
+      <Connecting message="Connecting..."/>
+    </template>
   </div>
 </template>
+
+<script>
+import Connecting from '@/components/Connecting.vue'
+import ServerService from '@/services/Server'
+
+export default {
+  name: 'App',
+  components: {
+    Connecting,
+  },
+  data () {
+    return {
+      server: {},
+    }
+  },
+  mounted () {
+    this.getServerStatus()
+  },
+  methods: {
+    getServerStatus () {
+      ServerService.fetchStatus()
+        .then((response) => {
+          this.server = response.data
+        })
+    }
+  }
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  @apply h-full;
 }
 </style>
