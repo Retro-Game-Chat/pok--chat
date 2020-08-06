8e70d89c226ea9a3249983da35feb199c196a00b
Greg Holmes <greg.holmes2@vonage.com>
2020-08-06 10:05:53 +0100

    tidied up CSS classes for Login form

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="Login">
-    <form class="w-full max-w-lg">
+    <form class="Login--form">
       <div class="flex flex-wrap -mx-3 mb-6">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-name">
+          <label class="Login--form-name-label" for="grid-name">
             Name
           </label>
-          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" :class="{ 'border-red-500': errors.length }" id="grid-name" type="text" ref="name" placeholder="Luke" v-model="name">
+          <input class="Login--form-name-input" :class="{ 'border-red-500': errors.length }" id="grid-name" type="text" ref="name" placeholder="Luke" v-model="name">
           <p v-for="(error, index) in errors" :key="`error-${index}`" class="text-red-500 text-xs italic">{{ error }}</p>
         </div>
       </div>
       <div class="flex flex-wrap -mx-3 mb-2">
         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-version">
+          <label class="Login--form-version-label" for="grid-version">
               Version
           </label>
           <div class="relative">	
@@ -34,7 +34,7 @@
 
       <div class="md:flex md:items-center">
         <div class="md:w-2/3">
-          <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" @click.prevent="submitted">
+          <button class="Login--button" type="button" @click.prevent="submitted">
             Sign Up
           </button>
         </div>
@@ -102,5 +102,37 @@ export default {
 <style scoped>
 .Login {
   @apply flex w-full h-full justify-center items-center;
+}
+
+.Login--form {
+  @apply w-full max-w-lg
+}
+
+.Login--form-name-label {
+  @apply block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2
+}
+
+.Login--form-name-input {
+  @apply appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight
+}
+
+.Login--form-name-input:focus {
+  @apply outline-none bg-white
+}
+
+.Login--form-version-label {
+  @apply block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2
+}
+
+.Login--button {
+  @apply shadow bg-purple-500 text-white font-bold py-2 px-4 rounded
+}
+
+.Login--button:hover {
+  @apply bg-purple-400
+}
+
+.Login--button:focus {
+  @apply shadow-outline outline-none
 }
 </style>
