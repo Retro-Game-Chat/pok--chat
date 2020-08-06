7f97c8f7d7e82536220771dd764de6a5464c61a9
Luke Oliff <luke@lukeoliff.com>
2020-08-06 08:32:27 +0100

    remove semicolon

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './assets/styles/index.css';
+import './assets/styles/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
