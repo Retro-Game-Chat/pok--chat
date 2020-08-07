93df582ae4d960d035cd2cd370ca6919105cdf91
Luke Oliff <luke@lukeoliff.com>
2020-08-07 22:52:04 +0100

    use private key as string not path

--- utils/nexmo.js
+++ utils/nexmo.js
@@ -6,5 +6,5 @@ module.exports = new Nexmo({
   apiKey: process.env.VONAGE_API_KEY,
   apiSecret: process.env.VONAGE_API_SECRET,
   applicationId: process.env.VONAGE_APPLICATION_ID,
-  privateKey: process.env.VONAGE_PRIVATE_KEY_PATH
+  privateKey: process.env.VONAGE_PRIVATE_KEY
 }, options);
