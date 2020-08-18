2407f0b9d8318d7eb1484eebcc2c8143f6865b62
Luke Oliff <luke@lukeoliff.com>
2020-08-18 10:53:45 +0100

    interactive char customisation form

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,57 +1,56 @@
 <template>
   <div class="PokéBox CharacterCustomisation">
-    <div class="w-1/3 h-auto my-auto" style="zoom:250%">
-      <div class="m-auto overflow-y-visible" :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/body/${body}.gif') 0rem 0rem`">
-        <div :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/torso/${torso}.gif') 0rem 0rem`">
-          <div :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/legs/${legs}.gif') 0rem 0rem`">
-            <div :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/head/${head}.gif') 0rem 0rem`"></div>
+    <div class="w-1/3 h-auto my-auto" style="zoom: 150%">
+      <div class="m-auto overflow-y-visible" :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/body/${data.body}.gif`)}') 0rem 0rem`">
+        <div :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/torso/${data.torso}.gif`)}') 0rem 0rem`">
+          <div :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/legs/${data.legs}.gif`)}') 0rem 0rem`">
+            <div :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/head/${data.head}.gif`)}') 0rem 0rem`"></div>
           </div>
         </div>
       </div>
-      <div class="mx-auto mt-2" :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/body/${body}.gif') -4rem 0rem`">
-        <div :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/torso/${torso}.gif') -4rem 0rem`">
-          <div :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/legs/${legs}.gif') -4rem 0rem`">
-            <div :style="`width: 1rem; height: 1rem; background: no-repeat url('/artwork/gif/head/${head}.gif') -4rem 0rem`"></div>
+      <div class="mx-auto mt-2" :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/body/${data.body}.gif`)}') -4rem 0rem`">
+        <div :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/torso/${data.torso}.gif`)}') -4rem 0rem`">
+          <div :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/legs/${data.legs}.gif`)}') -4rem 0rem`">
+            <div :style="`width: 1rem; height: 1rem; background: no-repeat url('${require(`@/assets/artwork/gif/head/${data.head}.gif`)}') -4rem 0rem`"></div>
           </div>
         </div>
-      </div>
-      <div class="mx-auto mt-2 w-4 h-4 border">
-        <svg>
-          <use xlink:href="/artwork/svg/Down.svg" />
-        </svg>
       </div>
     </div>
     <div class="w-2/3 text-right">
       <div class="py-2 leading-8">
         <input 
           class="bg-gray-200 outline-none text-center w-full"
+          :class="{ 'border border-red-500': errors.name }"
           type="text"
           v-model="name"
           placeholder="What's your name?"
         >
       </div>
+      <div class="flex py-2 text-red-500" v-if="errors.name">
+        {{ errors.name }}
+      </div>
       <div class="py-2 flex">
         <button 
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-l"
-          @click.prevent="head = head-1 > 1 ? head-1 : 1"
+          @click.prevent="data.head = data.head-1 > 1 ? data.head-1 : 1"
         >
           &lt;
         </button>
         <input 
           class="bg-gray-200 outline-none text-center flex-auto"
           type="text"
-          :value="`Head (${head})`"
+          :value="`Head (${data.head})`"
           @focus="$event.target.blur()"
         >
         <button
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-r"
-          @click.prevent="head = head+1 < headEnd ? head+1 : headEnd"
+          @click.prevent="data.head = data.head+1 < headEnd ? data.head+1 : headEnd"
         >
           &gt;
         </button>
         <button
           class="font-bold py-2 text-center flex px-3 ml-2 text-base"
-          @click.prevent="head = random(1, headEnd)"
+          @click.prevent="data.head = random(1, headEnd)"
         >
           ↻
         </button>
@@ -59,25 +58,25 @@
       <div class="py-2 flex">
         <button 
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-l"
-          @click.prevent="torso = torso-1 > 1 ? torso-1 : 1"
+          @click.prevent="data.torso = data.torso-1 > 1 ? data.torso-1 : 1"
         >
           &lt;
         </button>
         <input 
           class="bg-gray-200 outline-none text-center flex-auto"
           type="text"
-          :value="`Torso (${torso})`"
+          :value="`Torso (${data.torso})`"
           @focus="$event.target.blur()"
         >
         <button
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-r"
-          @click.prevent="torso = torso+1 < torsoEnd ? torso+1 : torsoEnd"
+          @click.prevent="data.torso = data.torso+1 < torsoEnd ? data.torso+1 : torsoEnd"
         >
           &gt;
         </button>
         <button
           class="font-bold py-2 text-center flex px-3 ml-2 text-base"
-          @click.prevent="torso = random(1, torsoEnd)"
+          @click.prevent="data.torso = random(1, torsoEnd)"
         >
           ↻
         </button>
@@ -85,25 +84,25 @@
       <div class="py-2 flex">
         <button 
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-l"
-          @click.prevent="legs = legs-1 > 1 ? legs-1 : 1"
+          @click.prevent="data.legs = data.legs-1 > 1 ? data.legs-1 : 1"
         >
           &lt;
         </button>
         <input 
           class="bg-gray-200 outline-none text-center flex-auto"
           type="text"
-          :value="`Legs (${legs})`"
+          :value="`Legs (${data.legs})`"
           @focus="$event.target.blur()"
         >
         <button
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-r"
-          @click.prevent="legs = legs+1 < legsEnd ? legs+1 : legsEnd"
+          @click.prevent="data.legs = data.legs+1 < legsEnd ? data.legs+1 : legsEnd"
         >
           &gt;
         </button>
         <button
           class="font-bold py-2 text-center flex px-3 ml-2 text-base"
-          @click.prevent="legs = random(1, legsEnd)"
+          @click.prevent="data.legs = random(1, legsEnd)"
         >
           ↻
         </button>
@@ -111,25 +110,25 @@
       <div class="py-2 flex">
         <button 
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-l"
-          @click.prevent="body = body-1 > 1 ? body-1 : 1"
+          @click.prevent="data.body = data.body-1 > 1 ? data.body-1 : 1"
         >
           &lt;
         </button>
         <input 
           class="bg-gray-200 outline-none text-center flex-auto"
           type="text"
-          :value="`Body (${body})`"
+          :value="`Body (${data.body})`"
           @focus="$event.target.blur()"
         >
         <button
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 flex rounded-r"
-          @click.prevent="body = body+1 < bodyEnd ? body+1 : bodyEnd"
+          @click.prevent="data.body = data.body+1 < bodyEnd ? data.body+1 : bodyEnd"
         >
           &gt;
         </button>
         <button
           class="font-bold py-2 text-center flex px-3 ml-2 text-base"
-          @click.prevent="body = random(1, bodyEnd)"
+          @click.prevent="data.body = random(1, bodyEnd)"
         >
           ↻
         </button>
@@ -138,16 +137,17 @@
         <button
           class="bg-gray-100 hover:bg-gray-200 text-gray-800 font-bold py-2 text-center w-1/2 px-3 mr-2"
           @click.prevent="
-            head = random(1, headEnd);
-            torso = random(1, torsoEnd);
-            legs = random(1, legsEnd);
-            body = random(1, bodyEnd);
+            data.head = random(1, headEnd);
+            data.torso = random(1, torsoEnd);
+            data.legs = random(1, legsEnd);
+            data.body = random(1, bodyEnd);
           "
         >
           <span class="text-base">↻</span> Random
         </button>
         <button
           class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold w-1/2 text-center rounded"
+          @click.prevent="submit"
         >
           Submit
         </button>
@@ -157,47 +157,44 @@
 </template>
 
 <script>
-const bodies = {
-  1: {
-    color: '#ffdc5d'
-  },
-  2: {
-    color: '#7c533e'
-  },
-  3: {
-    color: '#af7e57'
-  },
-  4: {
-    color: '#d5ab88'
-  },
-  5: {
-    color: '#f3d2a2'
-  },
-  6: {
-    color: '#f7dece'
-  },
-}
-
-console.log(bodies)
-
 export default {
   name: 'Test',
 
   data() {
     return {
+      errors: {},
       name: null,
-      head: 1,
+      data: {
+        head: 1,
+        torso: 1,
+        legs: 1,
+        body: 1,
+      },
       headEnd: 69,
-      torso: 1,
       torsoEnd: 2,
-      legs: 1,
       legsEnd: 2,
-      body: 1,
       bodyEnd: 6,
     }
   },
 
   methods: {
+    submit () {
+      this.errors = {}
+
+      if (!this.name) {
+        this.errors['name'] = 'Name required'
+
+        // console.log(this.errors)
+      }
+
+      if (Object.keys(this.errors).length === 0) {
+        console.log({
+          name: this.name,
+          data: this.data
+        })
+      } 
+    },
+
     random (min, max) {
       min = Math.ceil(min)
       max = Math.floor(max)
@@ -211,6 +208,6 @@ export default {
 <style scoped>
 .CharacterCustomisation {
   @apply mt-10 flex;
-  width: 30rem
+  width: 35rem
 }
 </style>
